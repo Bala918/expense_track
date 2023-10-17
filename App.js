feaee451655116e91ad73628f4d2b0a17690b1ff
bala918 <krishnanbk195@gmail.com>
2023-10-17 11:46:31 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+var [name, setName] = useState("Bala")
+const MyComponent = () => {
+  const update_name = ()=>{
+    setName = "Krishnan"
+  }  
+  return (    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container2}>
+        <Text>
+          {name}
+        </Text>
+        <button onPress={name} />
+      </View>                 
+    </View>    
   );
-}
+};
+
+export default MyComponent;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container : {
+    flex:1,
+    justifyContent:'center',
+    backgroundColor:"lightblue",
+    borderWidth:10,
+    borderColor:"red",        
   },
-});
+  container2 : {        
+    margin:16,
+    borderWidth:1,
+    borderColor:"blue",     
+    alignItems:"center"    
+  }  
+})
